89ccf8b61ed12d72be4e45ff9517d8ba51d54ddd
Elena <elena.demeneva.90@mail.ru>
2022-11-12 17:05:51 +0500

    Add TAsk 10

---
+++ Task10/Program.cs
@@ -0,0 +1,13 @@
+﻿// Упражнение 3
+// Напишите программу, которая выводит на консоль таблицу умножения
+
+int result;
+for (int i = 1; i < 11; i++)
+{
+    for (int j = 1; j < 11; j++)
+    {
+        result = i * j;
+        Console.WriteLine($"{i} * {j} = {result}\t");
+    }
+    Console.WriteLine();
+}
