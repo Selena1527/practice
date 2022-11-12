02727c0dceeba6ab59159991c9df97dd9bae0e76
Elena <elena.demeneva.90@mail.ru>
2022-11-12 14:22:02 +0500

    Add Task 7

---
+++ Task7/Program.cs
@@ -0,0 +1,34 @@
+﻿// Упражнение 7
+// Измените предыдущую программу. 
+// Пусть пользователь кроме номера операции вводит два числа, 
+// и в зависимости от номера операции с введенными числами 
+// выполняются определенные действия (например, при вводе числа 3 числа умножаются). 
+// Результа операции выводиться на консоль.
+
+Console.WriteLine("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите номер операции: 1.Сложение  2.Вычитание  3.Умножение");
+ 
+int operation = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+switch (operation)
+{
+    case 1:
+        result = num1 + num2;
+        Console.WriteLine($"Результат операции {result}");
+    break;
+    case 2:
+        result = num1 - num2;
+        Console.WriteLine($"Результат операции {result}");
+    break;
+    case 3:
+        result = num1 * num2;
+        Console.WriteLine($"Результат операции {result}");
+    break;
+    default:
+        Console.WriteLine("Неизвестная операция");
+    break;
+}
