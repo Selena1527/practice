dca8f1e65a383890cd4037c5aca4d585c88a44a3
Elena <elena.demeneva.90@mail.ru>
2022-11-12 17:56:42 +0500

    Add TAsk 11

---
+++ Task11/Program.cs
@@ -0,0 +1,62 @@
+﻿// int[] numbers = { 1, 2, 3, 4, 5 };
+// foreach (int i in numbers)
+// {
+//     Console.WriteLine(i);
+// }
+
+// int[] numbers = { 1, 2, 3, 4, 5 };
+// for (int i = 0; i < numbers.Length; i++)
+// {
+//     numbers[i] = numbers[i] * 2;
+//     Console.WriteLine(numbers[i]);
+// }
+
+// int[] numbers = { 1, 2, 3, 4, 5 };
+// int i = 0;
+// while(i < numbers.Length)
+// {
+//     Console.WriteLine(numbers[i]);
+//     i++;
+// }
+
+
+// int[,] numbers = { { 1, 2, 3 }, { 4, 5, 6 }};
+ 
+// int rows = numbers.GetUpperBound(0) + 1;    // количество строк
+// int columns = numbers.Length / rows;        // количество столбцов
+// // или так
+// // int columns = numbers.GetUpperBound(1) + 1;
+ 
+// for (int i = 0; i < rows; i++)
+// {
+//     for (int j = 0; j < columns; j++)
+//     {
+//         Console.Write($"{numbers[i, j]} \t");
+//     }
+//     Console.WriteLine();
+// }
+
+
+int[][] numbers = new int[3][];
+numbers[0] = new int[] { 1, 2 };
+numbers[1] = new int[] { 1, 2, 3 };
+numbers[2] = new int[] { 1, 2, 3, 4, 5 };
+// foreach(int[] row in numbers)
+// {
+//     foreach(int number in row)
+//     {
+//         Console.Write($"{number} \t");
+//     }
+//     Console.WriteLine();
+// }
+
+
+// перебор с помощью цикла for
+for (int i = 0; i<numbers.Length;i++)
+{
+    for (int j =0; j<numbers[i].Length; j++)
+    {
+        Console.Write($"{numbers[i][j]} \t");
+    }
+    Console.WriteLine();
+}
